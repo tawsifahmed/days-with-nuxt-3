04fa918168a86cdf6a9202c8b811f066afe02123
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2023-11-23 13:10:20 +0600

    route parameter

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>This is a simple about page.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Prodcuts</p>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
