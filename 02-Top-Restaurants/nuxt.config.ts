dd9c8db633a113d47c8d9deda778991802a3d17e
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-02-01 13:06:26 +0600

    second project initiated

---
+++ 02-Top-Restaurants/nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Top Restaurants",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "",
+        },
+        // add more meta tags here
+        { name: "theme-color", content: "#ffffff" }
+      ],
+    },
+  },
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
