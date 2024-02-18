826f52c29ce88df3ece7d453f640715ef9ccbcd2
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-02-18 13:16:51 +0600

    02 restaurants done

---
+++ 02-Top-Restaurants/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <Nav/>
+      <div class="container">
+        <h1 class="text-3xl font-bold mt-10 mb-2">Page Not Found</h1>
+        <NuxtLink to="/restaurants" class="font-semibold underline underline-offset-1 text-slate-700">Go Back</NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  
+  <style scoped>
+  .container{
+    text-align: center;
+  }
+  </style>
+  
